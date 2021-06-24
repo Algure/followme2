3c3f1df67d125f682dfdb92c2f3ac41342680dc6
Algure <ajirie98@gmail.com>
2021-06-24 17:21:46 +0100

    maintenance update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,9 +40,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "47bce36cf20ef7551526526b8b9459c8",
-"/": "47bce36cf20ef7551526526b8b9459c8",
-"main.dart.js": "e63e46a4188f797e9e25bef3e53713ff",
+"index.html": "aca056335ccbe5718a8fa0bbb66529b6",
+"/": "aca056335ccbe5718a8fa0bbb66529b6",
+"main.dart.js": "7d1fc8de90a286245ac92f45e7e4589b",
 "manifest.json": "fcb93ade9211b7b4fb164f7cc217fc1b",
 "version.json": "111adebc93e9b7fca8508702eb9e22a4"
 };
